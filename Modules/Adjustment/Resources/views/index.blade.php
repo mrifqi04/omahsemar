b71865e3c1bb83c45309a93bebae7d1c416ae036
Mohammad Akmal Rifqi <akmal@massivemusic.co.id>
2025-09-07 11:59:35 +0700

    fix: change name breadcrumb

--- Modules/Adjustment/Resources/views/index.blade.php
+++ Modules/Adjustment/Resources/views/index.blade.php
@@ -9,7 +9,7 @@
 @section('breadcrumb')
     <ol class="breadcrumb border-0 m-0">
         <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-        <li class="breadcrumb-item active">Adjustments</li>
+        <li class="breadcrumb-item active">Adjustments asdasd</li>
     </ol>
 @endsection
 
