d80049c9a0bef62919ede0b40535da311141fcca
akmal <satoefrekuensi@gmail.com>
2025-09-21 23:26:37 +0700

    feat: add new menu

--- Modules/StockOut/Entities/Stockout.php
+++ Modules/StockOut/Entities/Stockout.php
@@ -4,6 +4,8 @@ namespace Modules\Stockout\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\DeliveryNote\Entities\DeliveryNote;
+use Modules\Stockout\Entities\StockoutDetail;
 
 class Stockout extends Model
 {
